a7ac576af0692c2900baff3d776f28a8a8bc9acf
Oliver Suarez <oliver.suareztinoco@gmail.com>
2026-04-06 02:07:14 -0500

    mensaje

--- PROVIAS/migracion/query15.sql
+++ PROVIAS/migracion/query15.sql
@@ -1,4 +1,4 @@
-OBSERVACIONES PARA que se corra antes que el query8
+-- OBSERVACIONES PARA que se corra antes que el query8
 
 -- exec sys.sp_spaceused 'Tramite.ExpedienteBloqueadoPersonaVisualiza'
 -- select*into Tramite.ExpedienteBloqueadoPersonaVisualiza_historico
