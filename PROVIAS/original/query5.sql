fe1dd4874b090f2ab1f0fb64e559feb8aee859c5
Servicio de Administracion de Base de Datos OTI5 <sbdatosOTI5@proviasnac.dom>
2026-05-22 20:24:08 -0500

    modificar query5
    Detalle de la modificacion del query5

--- PROVIAS/original/query5.sql
+++ PROVIAS/original/query5.sql
@@ -31,40 +31,15 @@ ALTER PROCEDURE [Tramite].[paListarExpedientePendienteJefaturaPorRecibirFosCad]
 	@pFlgBusqueda int
 AS
 BEGIN TRY
-
 	DECLARE @vIdAreaJefe int=0
 	DECLARE @vIdEmpresaJefe int=0
 	DECLARE @vIdCargoJefe int=0
-	--DECLARE @vTablaCargos tABLE(Idcargo int)
-	--INSERT INTO @vTablaCargos
-	--SELECT IdCargo FROM General.Cargo WHERE IdCatalogoTipoCargo in (32,33,34)
 
 	if @pIdPeriodo = 0
 	set @pIdPeriodo = year(getdate())
 
 	SELECT @vIdAreaJefe=IdArea,@vIdEmpresaJefe=IdEmpresa FROM RecursoHumano.visPersonaJefe where IdArea=@pIdArea
-	--DECLARE @vTablaExpediente TABLE(IdExpediente int)
 	DECLARE @vTablaExpediente TABLE(IdExpediente int, FechaMovimiento Datetime,EsParaAnular int,DiasPendiente int,NombrePersonaOrigen varchar(max),NumeroDocumento varchar(max),IdExpedienteDocumento int);
-	--DECLARE @MITABLA TABLE (
-	--IdExpediente int,
-	--ExpedienteConfidencial bit,
-	--NTFechaExpediente varchar (10),
-	--HoraExpediente varchar (5),
-	--IdCatalogoTipoPrioridad int,
-	--CatalogoTipoPrioridad varchar (100),
-	--CatalogoTipoTramite varchar (100),
-	--ColorCatalogoTipoTramite varchar (100),
-	--Logueo varchar (100),
-	--RutaFotoPersona varchar (100),
-	--AsuntoExpediente varchar (8000),
-	--NumeroFoliosExpediente int,
-	--ObservacionesExpediente varchar(4000),
-	--Fecha VARCHAR(20),
-	--NombreExpediente varchar (100),
-	--NombreCompletoCreador varchar (100),
-	--NumeroExpediente int,
-	--IdExpedienteSeguimiento int,
-	--FechaMovimiento datetime);
 
 	IF ISNUMERIC(@pBusquedaGeneral)=1 OR @pBusquedaGeneral IS NULL OR @pBusquedaGeneral=''
 	BEGIN
@@ -94,42 +69,38 @@ BEGIN TRY
 		AND (E.NumeroExpediente =  @pBusquedaGeneral OR @pBusquedaGeneral IS NULL OR @pBusquedaGeneral=0)
 		group by E.IdExpediente
 	END
-	--INSERT INTO @MITABLA
+
 	select
 		(SELECT convert(varchar,count(*)) FROM @vTablaExpediente)+'¦'+
-		(select STUFF((
-		SELECT
+		(select STUFF((SELECT
 		'¬'+convert(varchar,tE.EsParaAnular),
 		'|'+convert(varchar,tE.DiasPendiente),
 		'|'+tE.NombrePersonaOrigen,
 		'|'+replace(tE.NumeroDocumento,'|',''),
 		'|'+convert(varchar,tE.IdExpedienteDocumento),
-		--Tramite.funObtenerExpedientesEnlazados(E.IdExpediente) NombreExpedientesEnlazados,
-		'|'+CASE WHEN ENP.ExEnlazadoPri<>'' THEN ENP.ExEnlazadoPri else ENS.ExEnlazadoSec END, --NombreExpedientesEnlazados,
-		'|'+CASE WHEN EE.cantEnlaces>0 THEN '1' ELSE '0' END, --EsPrincipalEnlace, --Tramite.funEsPrincipalEnlace(E.IdExpediente)EsPrincipalEnlace,
-		'|'+OID.CatalogoTipoOrigen, --Tramite.fnObtenerOrigenInicialDocumento(E.IdExpediente) CatalogoTipoOrigen,
+		'|'+CASE WHEN ENP.ExEnlazadoPri<>'' THEN ENP.ExEnlazadoPri else ENS.ExEnlazadoSec END,
+		'|'+CASE WHEN EE.cantEnlaces>0 THEN '1' ELSE '0' END,
+		'|'+OID.CatalogoTipoOrigen,
 		'|'+convert(varchar,E.IdExpediente),
 		'|'+convert(varchar,E.ExpedienteConfidencial),
 		'|'+E.NTFechaExpediente,
 		'|'+E.HoraExpediente,
 		'|'+convert(varchar,E.IdCatalogoTipoPrioridad),
-		'|'+COALESCE(CTP.Descripcion,''), --CatalogoTipoPrioridad,
-		'|'+COALESCE(CTT.Descripcion,''), --CatalogoTipoTramite,
-		'|'+COALESCE(CTT.Detalle,''), --ColorCatalogoTipoTramite,
+		'|'+COALESCE(CTP.Descripcion,''),
+		'|'+COALESCE(CTT.Descripcion,''),
+		'|'+COALESCE(CTT.Detalle,''),
 		'|'+US.Logueo,
-		'|'+COALESCE(Seguridad.funObtenerRutaFotoPorIdPersona(E.IdPersonaCreador),'sinfotoH.jpg'), --RutaFotoPersona,
-		'|'+UPPER(replace(E.AsuntoExpediente,'|',' ')), --AsuntoExpediente,
-		'|'+convert(varchar,COALESCE(E.NumeroFoliosExpediente,0)), --NumeroFoliosExpediente,
-		'|'+COALESCE(replace(E.ObservacionesExpediente,'|',' '),''), --ObservacionesExpediente,
-		'|'+CONCAT(E.NTFechaExpediente ,' ', E.HoraExpediente), --Fecha,
-		'|'+CONCAT(SD.AbreviaturaSerieDocumentalExpediente +RIGHT('000000'+CONVERT(VARCHAR,E.NumeroExpediente),6),'-', E.IdPeriodo), --NombreExpediente,
-		'|'+CASE WHEN COALESCE(E.NombreCompletoCreador,'')<>'' THEN COALESCE(E.NombreCompletoCreador,'') ELSE PE.NombreCompleto END, --NombreCompletoCreador,
+		'|'+COALESCE(Seguridad.funObtenerRutaFotoPorIdPersona(E.IdPersonaCreador),'sinfotoH.jpg'),
+		'|'+UPPER(replace(E.AsuntoExpediente,'|',' ')),
+		'|'+convert(varchar,COALESCE(E.NumeroFoliosExpediente,0)),
+		'|'+COALESCE(replace(E.ObservacionesExpediente,'|',' '),''),
+		'|'+CONCAT(E.NTFechaExpediente ,' ', E.HoraExpediente),
+		'|'+CONCAT(SD.AbreviaturaSerieDocumentalExpediente +RIGHT('000000'+CONVERT(VARCHAR,E.NumeroExpediente),6),'-', E.IdPeriodo),
+		'|'+CASE WHEN COALESCE(E.NombreCompletoCreador,'')<>'' THEN COALESCE(E.NombreCompletoCreador,'') ELSE PE.NombreCompleto END,
 		'|'+convert(varchar,E.NumeroExpediente),
-		'|'+convert(varchar,COALESCE(ES.IdExpedienteSeguimiento,0)), --IdExpedienteSeguimiento,
-		--Tramite.funObtenerFechaMovimientoEnExpediente(E.IdExpediente,@vIdAreaJefe,@pIdCatalogoSituacionMovimientoDestino) FechaMovimiento
-		'|'+isnull(FORMAT(tE.FechaMovimiento, 'dd/MM/yyyy HH:mm'),'') --'|'+convert(varchar,tE.FechaMovimiento,21)
-	FROM
-	Tramite.Expediente E WITH (NOLOCK)
+		'|'+convert(varchar,COALESCE(ES.IdExpedienteSeguimiento,0)),
+		'|'+isnull(FORMAT(tE.FechaMovimiento, 'dd/MM/yyyy HH:mm'),'')
+	FROM Tramite.Expediente E WITH (NOLOCK)
 	INNER JOIN Seguridad.Usuario US ON US.IdUsuario=E.IdUsuarioCreacionAuditoria AND E.EstadoAuditoria=1  AND COALESCE(E.ExpedienteAnulado,0)=0
 	INNER JOIN Tramite.SerieDocumentalExpediente SD WITH (NOLOCK) ON SD.IdSerieDocumentalExpediente=E.IdSerieDocumentalExpediente
 	INNER JOIN Tramite.Catalogo CTP ON CTP.IdCatalogo=E.IdCatalogoTipoPrioridad
@@ -182,9 +153,44 @@ BEGIN TRY
 	OFFSET (@pNumeroPagina-1)*@pDimensionPagina ROWS
 	FETCH NEXT @pDimensionPagina ROWS ONLY
 	FOR XML PATH('')), 1, 1, ''))
+
 END TRY
 BEGIN CATCH
 	DECLARE @ERROR_NUMBER INT, @ERROR_SEVERITY INT,@ERROR_STATE INT,@ERROR_LINE INT,@ERROR_PROCEDURE VARCHAR(MAX)	,@ERROR_MESSAGE VARCHAR(MAX)
 	SELECT @ERROR_NUMBER=ERROR_NUMBER() , @ERROR_SEVERITY=ERROR_SEVERITY() , @ERROR_STATE=ERROR_STATE() , @ERROR_PROCEDURE='Tramite.paListarExpedientePendienteJefaturaPorRecibir',@ERROR_LINE=ERROR_LINE(),@ERROR_MESSAGE=ERROR_MESSAGE()
 	EXEC Seguridad.paGuardarErroresEnTablaLog @ERROR_NUMBER , @ERROR_SEVERITY , @ERROR_STATE ,  @ERROR_PROCEDURE,@ERROR_LINE,@ERROR_MESSAGE, @pIdUsuarioAuditoria
 END CATCH
+go
+
+
+exec Tramite.paListarExpedientePendienteJefaturaPorRecibirFosCad
+@pConFiltroFecha=0,
+@pFechaInicio='22/05/2026',
+@pFechaFin='22/05/2026',
+@pConFiltroFechaMovimiento=1,
+@pFechaInicioMovimiento='22/05/2026',
+@pFechaFinMovimiento='22/05/2026',
+@pIdArea=79,
+@pIdCatalogoSituacionMovimientoDestino=4,
+@pTipoSituacionMovimiento=4,
+@pIdAreaOrigen=0,
+@pIdAreaDestino=0,
+@pIdPeriodo=2026,
+@pIdCatalogoTipoPrioridad=0,
+@pIdCatalogoTipoTramite=0,
+@pIdCatalogoTipoDocumento=0,
+@pNumeroExpediente='',
+@pNumeroDocumento='',
+@pPersonaDesde='',
+@pPersonaPara='',
+@pIdTipoIngreso=0,
+@pFechaDocumento='',
+@pEmisorExpediente='',
+@pAsuntoExpediente='',
+@pIdUsuarioAuditoria=349,
+@pCampoOrdenado=NULL,
+@pTipoOrdenacion=NULL,
+@pNumeroPagina=1,
+@pDimensionPagina=10,
+@pBusquedaGeneral=NULL,
+@pFlgBusqueda=0
